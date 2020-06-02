194c059b508a8333542f44e9d89d23116652b640
farshmachine <m4ghty@yandex.ru>
2020-06-02 18:13:38 +0300

    Updates

---
+++ precache-manifest.1bb7f1e16d67848cef87d3bcb5fd025d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6530a5755d371aa0431cadd09d4a692d",
+    "url": "/todo/index.html"
+  },
+  {
+    "revision": "9bd1fb55f8833244b152",
+    "url": "/todo/static/css/main.b6bdc0e2.chunk.css"
+  },
+  {
+    "revision": "bfd8f71a3a88d776b646",
+    "url": "/todo/static/js/2.c3ab7d37.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo/static/js/2.c3ab7d37.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9bd1fb55f8833244b152",
+    "url": "/todo/static/js/main.eefe2e23.chunk.js"
+  },
+  {
+    "revision": "05368ca9ae402c114881",
+    "url": "/todo/static/js/runtime-main.2da2b585.js"
+  }
+]);
